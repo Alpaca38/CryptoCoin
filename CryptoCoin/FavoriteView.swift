bfc076e058ad2565ee1d9e383e49061951aa7818
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-09-06 16:51:31 +0900

    즐겨찾기 화면 더미데이터로 UI 구성

--- CryptoCoin/FavoriteView.swift
+++ CryptoCoin/FavoriteView.swift
@@ -9,12 +9,75 @@ import SwiftUI
 
 struct FavoriteView: View {
     @Binding var likedCoinIDs: [String]
+    @State private var list: MarketResponse = [
+        MarketItem(id: "1", name: "", symbol: "", image: "", currentPrice: 0, priceChangePercentage24h: 0, low24h: 0, high24h: 0, ath: 0, athDate: "", atl: 0, atlDate: "", lastUpdated: "", sparklineIn7d: SparkLine(price: [])),
+        MarketItem(id: "2", name: "", symbol: "", image: "", currentPrice: 0, priceChangePercentage24h: 0, low24h: 0, high24h: 0, ath: 0, athDate: "", atl: 0, atlDate: "", lastUpdated: "", sparklineIn7d: SparkLine(price: [])),
+        MarketItem(id: "3", name: "", symbol: "", image: "", currentPrice: 0, priceChangePercentage24h: 0, low24h: 0, high24h: 0, ath: 0, athDate: "", atl: 0, atlDate: "", lastUpdated: "", sparklineIn7d: SparkLine(price: []))
+    ]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                FavoriteCoinGridView(list: $list)
+                    .padding()
+            }
+            .navigationTitle("Favorite Coin")
+        }
     }
 }
 
+private struct FavoriteCoinGridView: View {
+    @Binding var list: MarketResponse
+    
+    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
+    var body: some View {
+        LazyVGrid(columns: columns, content: {
+            ForEach(list, id: \.id) { item in
+                coinItem(item)
+            }
+        })
+    }
+    
+    func coinItem(_ item: MarketItem) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(.white)
+                .frame(height: 150)
+                .shadow(radius: 5)
+            VStack {
+                HStack {
+                    Image(systemName: "person")
+                    
+                    VStack(alignment: .leading) {
+                        Text("bitcoin")
+                            .bold()
+                        Text("BTC")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text(52413424.formatted(.currency(code: "krw")))
+                        .bold()
+                    Text(String(0.64) + "%")
+                        .font(.caption)
+                        .bold()
+                        .foregroundStyle(.red)
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 10)
+                        .background(.red.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            .padding()
+        }
+    }
+}
+//
 //#Preview {
 //    FavoriteView()
 //}
---
+++ CryptoCoin/MarketResponse.swift
@@ -0,0 +1,45 @@
+//
+//  MarketResponse.swift
+//  CryptoCoin
+//
+//  Created by 조규연 on 9/6/24.
+//
+
+import Foundation
+
+struct MarketItem: Decodable {
+    let id: String
+    let name: String
+    let symbol: String
+    let image: String
+    let currentPrice: Double
+    let priceChangePercentage24h: Double
+    let low24h: Double
+    let high24h: Double
+    let ath: Double
+    let athDate: String
+    let atl: Double
+    let atlDate: String
+    let lastUpdated: String
+    let sparklineIn7d: SparkLine
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name, symbol, image
+        case currentPrice = "current_price"
+        case priceChangePercentage24h = "price_change_percentage_24h"
+        case low24h = "low_24h"
+        case high24h = "high_24h"
+        case ath
+        case athDate = "ath_date"
+        case atl
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7d = "sparkline_in_7d"
+    }
+}
+
+typealias MarketResponse = [MarketItem]
+
+struct SparkLine: Decodable {
+    let price: [Double]
+}
