b422521885496293c11dddf9278733abae38ae7a
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-09-06 13:39:29 +0900

    Initial Commit

---
+++ CryptoCoin/APIKey.swift
@@ -0,0 +1,8 @@
+//
+//  APIKey.swift
+//  CryptoCoin
+//
+//  Created by 조규연 on 9/6/24.
+//
+
+import Foundation
---
+++ CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by 조규연 on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
