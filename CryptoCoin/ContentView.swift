658453c399335ec6b1de7b38fd029ac034c958fd
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-09-08 02:32:37 +0900

    트렌딩 화면 즐겨찾기 섹션 구현

--- CryptoCoin/ContentView.swift
+++ CryptoCoin/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            TrendingView()
+            TrendingView(likedCoinIDs: $likedCoinIDs)
                 .tabItem {
                     Image(systemName: "chart.line.uptrend.xyaxis")
                 }
--- CryptoCoin/TrendingView.swift
+++ CryptoCoin/TrendingView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct TrendingView: View {
+    @Binding var likedCoinIDs: [String]
+    @State private var favoriteList: MarketResponse = []
+    
     var body: some View {
         NavigationView {
             sections()
                 .navigationTitle("Crypto Coin")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Image(systemName: "person")
+                            .asButton {
+                                print("프로필 버튼")
+                            }
+                    }
+                }
+        }
+        .task {
+            do {
+                if likedCoinIDs.count < 2 {
+                    favoriteList.removeAll()
+                } else {
+                    let result = try await CoingeckoAPI.shared.getMarketData(ids: likedCoinIDs)
+                    favoriteList = result
+                }
+            } catch {
+                print(error)
+            }
         }
     }
     
     func sections() -> some View {
         ScrollView {
             LazyVStack(spacing: 40) {
-                FavoriteSection()
+                if favoriteList.count >= 2 {
+                    FavoriteSection(list: favoriteList, likedCoinIDs: $likedCoinIDs)
+                }
                 CoinSection()
                 NFTSection()
             }
@@ -27,7 +52,9 @@ struct TrendingView: View {
 }
 
 private struct FavoriteSection: View {
-    let item = MarketItem(id: "a", name: "Bitcoin", symbol: "btc", image: "", currentPrice: 0, priceChangePercentage24h: 0, low24h: 0, high24h: 0, ath: 0, athDate: "", atl: 0, atlDate: "", lastUpdated: "", sparklineIn7d: nil)
+    let list: MarketResponse
+    @Binding var likedCoinIDs: [String]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Text("My Favorite")
@@ -35,17 +62,22 @@ private struct FavoriteSection: View {
                 .font(.title)
             ScrollView(.horizontal) {
                 LazyHStack(spacing: 15) {
-                    favoriteItem()
-                    favoriteItem()
-                    favoriteItem()
+                    ForEach(list, id: \.id) { item in
+                        NavigationLink {
+                            NavigationLazyView(ChartView(likedCoinIDs: $likedCoinIDs, id: item.id))
+                        } label: {
+                            favoriteItem(item)
+                        }
+                    }
                 }
             }
+            .scrollIndicators(.hidden)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
     }
     
-    func favoriteItem() -> some View {
+    func favoriteItem(_ item: MarketItem) -> some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25.0)
                 .fill(.gray.opacity(0.2))
@@ -81,6 +113,7 @@ private struct CoinSection: View {
             ScrollView(.horizontal) {
                 CoinGridView()
             }
+            .scrollIndicators(.hidden)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
@@ -98,6 +131,7 @@ private struct NFTSection: View {
             ScrollView(.horizontal) {
                 CoinGridView()
             }
+            .scrollIndicators(.hidden)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
@@ -137,7 +171,7 @@ struct CoinGridView: View {
         .padding(.vertical, 10)
     }
 }
-
-#Preview {
-    TrendingView()
-}
+//
+//#Preview {
+//    TrendingView()
+//}
